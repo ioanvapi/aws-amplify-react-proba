19a83fcb9b35b6e98fa557bdab8af58d1f81ef9e
vapirovschi <vapirovschi@arezzosky.com>
2021-09-16 10:07:15 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from AWS Amplify with React !!!
+          Hello from AWS Amplify with React App !!!
         </p>
         <a
           className="App-link"
